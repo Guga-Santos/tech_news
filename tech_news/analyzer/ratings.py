290455c4708326d814b17da46f467783573816c6
GugaSantos <guga.santos.guga@gmail.com>
2022-11-10 00:56:18 -0300

    feat: find for top five

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,14 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    db = get_collection()
+    list = db.find().sort([("comments_count", -1), ("title", 1)]).limit(5)
+    top_5 = [(news['title'], news['url']) for news in list]
+
+    return top_5
+
 
 
 # Requisito 11
